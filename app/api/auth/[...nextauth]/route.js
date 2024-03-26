4527dd13954a6cbbc8c142bca10094b582bb1ac2
fbragee <145098995+fbragee@users.noreply.github.com>
2024-03-26 11:55:47 +0000

    Added sign in functionality correctly and connected mongodb

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -2,6 +2,10 @@ import NextAuth from 'next-auth/next';
 
 import GoogleProvider from 'next-auth/providers/google';
 
+import { connectToDB } from '@utils/database';
+
+import User from '@models/user';
+
 const handler = NextAuth({
 	providers: [
 		GoogleProvider({
@@ -9,10 +13,35 @@ const handler = NextAuth({
 			clientSecret: process.env.GOOGLE_CLIENT_SECRET,
 		}),
 	],
-	async session({ session }) {},
-	async signIn({ profile }) {
-		try {
-		} catch (error) {}
+	callbacks: {
+		async session({ session }) {
+			const sessionUser = await User.findOne({ email: session.user.email });
+
+			session.user.id = sessionUser._id.toString();
+
+			return session;
+		},
+		async signIn({ profile }) {
+			try {
+				await connectToDB();
+
+				//check if a user already exists
+				const userExists = await User.findOne({ email: profile.email });
+
+				//if not, create a new user and save it to db
+				if (!userExists) {
+					await User.create({
+						email: profile.email,
+						username: profile.name.replace(' ', '').toLowerCase(),
+						image: profile.picture,
+					});
+				}
+				return true;
+			} catch (error) {
+				console.log(error);
+				return false;
+			}
+		},
 	},
 });
 
--- components/Nav.jsx
+++ components/Nav.jsx
@@ -6,17 +6,17 @@ import { useState, useEffect } from 'react';
 import { signIn, signOut, useSession, getProviders } from 'next-auth/react';
 
 const Nav = () => {
-	const isUserLoggedIn = true;
+	const { data: session } = useSession();
 
 	const [providers, setProviders] = useState(null);
 	const [toggleDrodown, setToggleDrodown] = useState(false);
 	useEffect(() => {
-		const setProviders = async () => {
+		const setUpProviders = async () => {
 			const response = await getProviders();
 
 			setProviders(response);
 		};
-		setProviders();
+		setUpProviders();
 	}, []);
 
 	return (
@@ -28,7 +28,7 @@ const Nav = () => {
 
 			{/* Desktop Navigation */}
 			<div className="sm:flex hidden">
-				{isUserLoggedIn ? (
+				{session?.user ? (
 					<div className="flex gap-3 md:gap-5">
 						<Link href="/create-prompt" className="black_btn">
 							Create Post
@@ -38,7 +38,7 @@ const Nav = () => {
 							Sign out
 						</button>
 						<Link href="/profile">
-							<Image src="/assets/images/logo.svg" width={37} height={37} className="rounded-full" alt="profile" />
+							<Image src={session?.user.image} width={37} height={37} className="rounded-full" alt="profile" />
 						</Link>
 					</div>
 				) : (
@@ -55,10 +55,10 @@ const Nav = () => {
 
 			{/* Mobile Navigation */}
 			<div className="sm:hidden flex relative">
-				{isUserLoggedIn ? (
+				{session?.user ? (
 					<div className="flex">
 						<Image
-							src="/assets/images/logo.svg"
+							src={session?.user.image}
 							width={37}
 							height={37}
 							className="rounded-full"

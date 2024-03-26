1d9249c3a4a31a52aca9cecffd0499bdb84edc19
fbragee <145098995+fbragee@users.noreply.github.com>
2024-03-26 19:11:32 +0000

    profile section show my posts

---
+++ app/api/prompt/[id]/route.js
@@ -0,0 +1,28 @@
+import Prompt from '@models/prompt';
+import { connectToDB } from '@utils/database';
+// GET (read)
+export const GET = async (request, { params }) => {
+	try {
+		await connectToDB();
+
+		const prompt = await Prompt.findById(params.id).populate('creator');
+		if (!prompt) return new Response('Prompt not found', { status: 404 });
+
+		return new Response(JSON.stringify(prompts), { status: 200 });
+	} catch (error) {
+		return new Response('Failed to fetch all prompts', { status: 500 });
+	}
+};
+
+// PATCH (update)
+export const PATCH = async (request, { params }) => {
+	const { prompt, tag } = await request.json();
+	try {
+		await connectToDB();
+
+		const existingPrompt = await Prompt.findById(params.id);
+
+		if (!existingPrompt) return new Response('Prompt not found', { status: 404 });
+	} catch (error) {}
+};
+// DELETE (delete)
